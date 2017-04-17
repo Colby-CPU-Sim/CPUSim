cd266b03db22143dfee34c81add31f0330e63d67
Kevin Brightwell <kevin.brightwell2@gmail.com>
2017-04-16 20:23:13 -0400

    Properly update Travis to handle JDK dependencies
    This allows the project to build and test on Travis.

---
+++ install-deps.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -ev
+
+case `uname` in
+    Darwin)     OS_NAME="osx"    ;;
+    Linux)      OS_NAME="linux"  ;;
+esac
+
+JDK_VERSION_LEVEL=${JDK_VERSION:(-1)}
+case ${OS_NAME} in
+    osx)
+        if [[ ${JDK_VERSION} == openjdk* ]]; then
+            echo "OpenJDK is not supported on OSX, defaulting to oraclejdk${JDK_VERSION_LEVEL}"
+            export JDK_VERSION=oraclejdk${JDK_VERSION_LEVEL}
+        fi
+
+        # Install the correct version of Java:
+
+        case ${JDK_VERSION_LEVEL} in
+            8)
+                CASK_PKG=caskroom/cask;
+                JAVA_PKG="java"
+                ;;
+            7|9)
+                CASK_PKG=caskroom/versions;
+                JAVA_PKG="java${JDK_VERSION_LEVEL}";
+                ;;
+        esac
+
+        echo "Installing ${JDK_VERSION} from: ${CASK_PKG}:${JAVA_PKG}"
+
+        brew tap ${CASK_PKG}
+
+        echo "brew update ..." ; brew update > /dev/null #; brew doctor; brew update
+
+        brew cask install ${JAVA_PKG}
+      ;;
+
+    linux)
+        export DISPLAY=:99.0
+        sh -e /etc/init.d/xvfb start
+      ;;
+esac
