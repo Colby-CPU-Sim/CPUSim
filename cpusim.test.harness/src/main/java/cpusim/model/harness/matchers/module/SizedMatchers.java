aa7b53cbe175633ed3323b025eb24cf68894a359
Kevin Brightwell <kevin.brightwell2@gmail.com>
2017-05-15 14:50:52 -0400

    Fix incorrect IntelliJ fix.

--- cpusim.test.harness/src/main/java/cpusim/model/harness/matchers/module/SizedMatchers.java
+++ cpusim.test.harness/src/main/java/cpusim/model/harness/matchers/module/SizedMatchers.java
@@ -4,10 +4,10 @@ import cpusim.model.module.Module;
 import cpusim.model.module.Sized;
 import cpusim.model.util.units.ArchValue;
 import org.hamcrest.Matcher;
+import org.hobsoft.hamcrest.compose.ComposeMatchers;
 import org.hobsoft.hamcrest.compose.ConjunctionMatcher;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.hobsoft.hamcrest.compose.ComposeMatchers.compose;
 import static org.hobsoft.hamcrest.compose.ComposeMatchers.hasFeature;
 
 /**
@@ -27,7 +27,10 @@ public abstract class SizedMatchers {
      */
     public static <T extends Module<T> & Sized<T>>
     ConjunctionMatcher<T> sized(T expected) {
-        return compose("sized", width(expected.getWidth()));
+        //noinspection RedundantTypeArguments
+        // IntelliJ is WRONG that this isn't needed. Java can't seem to coerce
+        // this property without the full spec.
+        return ComposeMatchers.<T>compose("sized", width(expected.getWidth()));
     }
 
     /** @see Sized#widthProperty() */
